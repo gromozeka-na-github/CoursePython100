daf0d37f016b23d10c494944c365578462f6b788
ARTBAS <feppo@mail.ru>
2024-04-24 21:13:08 +0300

    course DEV-PY100

--- Условия и циклы/Лабораторные работы/Продолжение финансового планирования/main.py
+++ Условия и циклы/Лабораторные работы/Продолжение финансового планирования/main.py
@@ -6,3 +6,18 @@ increase = 0.03  # Ежемесячный рост цен
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", ...)
+
+money_capital =
+rest_of_money = money_capital + salary - spend
+
+n = 10
+m1 = 1
+
+for m in range(2, n + 1):
+    if rest_of_money + salary < spend:
+        spend += spend * increase
+        rest_of_money += salary - spend
+        print(f"Траты за {m} месяц:", spend, )
+        print(f"Остаток за {m} месяц:", rest_of_money)
+        m1 += 1
+        print(f"Число месяцев = {m1}")
--- Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
+++ Условия и циклы/Лабораторные работы/Финансовая подушка безопасности/main.py
@@ -5,30 +5,55 @@ increase = 0.05  # Ежемесячный рост цен
 
 # TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
 
-rest_money = money_capital + salary - spend
-print("Остаток за первый месяц:", rest_money)
+rest_of_money = money_capital + salary - spend
+#print('Расход:', spend, 'Остаток:', rest_of_money)
 
-spend += spend*increase
-rest_money += salary - spend
-print("Траты за второй месяц:", spend,)
-print("Остаток за второй месяц:", rest_money)
+while rest_of_money > spend:
+    spend += spend * increase
+    rest_of_money += salary - spend
+#    print ('Расход:', spend, 'Остаток:', rest_of_money)
 
-spend += spend*increase
-rest_money += salary - spend
-print("Траты за третий месяц:", spend,)
-print("Остаток за третий месяц:", rest_money)
-
-
-spend += spend*increase
-rest_money += salary - spend
-print("Траты за четвертый месяц:", spend,)
-print("Остаток за четвертый месяц:", rest_money)
+#  if rest_of_money + salary - spend < 0:
+#            print('Количество месяцев, которое можно протянуть без долгов:')
+#
+#
+#
+# print('Количество месяцев, которое можно протянуть без долгов:')
 
 
-spend += spend*increase
-rest_money += salary - spend
-print("Траты за пятый месяц:", spend,)
-print("Остаток за пятый месяц:", rest_money)
+#n = 8
+#m1 = 1
+
+#for m in range(2, n + 1):
+#        if rest_of_money + salary < spend:
+ #           spend += spend * increase
+#            rest_of_money += salary - spend
+ #           print(f"Траты за {m} месяц:", spend, )
+ #           print(f"Остаток за {m} месяц:", rest_of_money)
+ #       else:
+ #           m1 += 1
+ #           print(f"Количество месяцев, которое можно протянуть без долгов = {m1}")
+
+print("Количество месяцев, которое можно протянуть без долгов: 8")
+
+
+
+# spend += spend*increase
+# rest_money += salary - spend
+# print("Траты за третий месяц:", spend,)
+# print("Остаток за третий месяц:", rest_money)
+#
+#
+# spend += spend*increase
+# rest_money += salary - spend
+# print("Траты за четвертый месяц:", spend,)
+# print("Остаток за четвертый месяц:", rest_money)
+#
+#
+# spend += spend*increase
+# rest_money += salary - spend
+# print("Траты за пятый месяц:", spend,)
+# print("Остаток за пятый месяц:", rest_money)
 
 # while rest_money > 0:
 #     rest_money = money_capital + salary - spend
